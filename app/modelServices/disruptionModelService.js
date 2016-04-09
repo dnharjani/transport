e29cc2c723309b94a28c86d66e464c0f2d3b7439
Danesh <dan24000@hotmail.com>
2016-04-09 01:22:29 +0100

    added update and delete

--- app/modelServices/disruptionModelService.js
+++ app/modelServices/disruptionModelService.js
@@ -8,7 +8,12 @@ function DisruptionModelService() {
 }
 
 DisruptionModelService.prototype.getDisruptions = function(){
-    return this.model.findAll({raw: true});
+    return this.model.findAll({
+        where: {
+            resolved: 0
+        },
+        raw: true
+    });
 };
 
 DisruptionModelService.prototype.getDisruptionById = function(disruptionId){
@@ -21,11 +26,27 @@ DisruptionModelService.prototype.getDisruptionById = function(disruptionId){
 };
 
 DisruptionModelService.prototype.addDisruption = function(lineId, fromStationId, toStationId, fromDate, toDate, reason){
-    return Promise.resolve();
+    return this.model.create({
+        lineId: lineId,
+        fromStationId: fromStationId,
+        toStationId: toStationId,
+        fromDate: fromDate,
+        toDate: toDate,
+        resolved: 0,
+        reason: reason
+    });
 };
 
 DisruptionModelService.prototype.removeDisruption = function(disruptionId){
-    return Promise.resolve();
+    return this.model.update(
+            {
+                resolved: 1
+            },
+            {
+            where: {
+                id: disruptionId
+            }
+        })
 };
 
 

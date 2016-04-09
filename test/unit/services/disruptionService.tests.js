eefce1cc936a772445883c75ecad2a850a56fd1b
Danesh <dan24000@hotmail.com>
2016-04-09 13:56:55 +0100

    tests

--- test/unit/services/disruptionService.tests.js
+++ test/unit/services/disruptionService.tests.js
@@ -1,10 +1,17 @@
 describe('Disruption Service tests', function () {
 
     var DisruptionModelServiceStub = {
-        getDisruptionById : sinon.stub()
+        getDisruptionById : sinon.stub(),
+        getDisruptions: sinon.stub(),
+        addDisruption: sinon.stub(),
+        removeDisruption: sinon.stub()
     };
 
     DisruptionModelServiceStub.getDisruptionById.returns(new Promise.resolve({id: 1}));
+    DisruptionModelServiceStub.getDisruptions.returns(new Promise.resolve([{id: 1}]));
+    DisruptionModelServiceStub.addDisruption.returns(new Promise.resolve({success: true}));
+    DisruptionModelServiceStub.removeDisruption.returns(new Promise.resolve({success: true}));
+
 
     var DisruptionService = proxyquire('../app/services/disruptionService', {
         '../modelServices/disruptionModelService': DisruptionModelServiceStub
@@ -18,12 +25,45 @@ describe('Disruption Service tests', function () {
         done();
     });
 
-    describe('Getting disruption by id', function () {
+    describe('getDisruptionById', function () {
         it('should call the model service', function (done) {
             DisruptionService.getDisruptionById(1).then(function(data){
                 expect(data).to.not.be.null;
                 expect(DisruptionModelServiceStub.getDisruptionById.calledOnce).to.be.true;
                 expect(DisruptionModelServiceStub.getDisruptionById.calledWith(1)).to.be.true;
+                done();
+            })
+        });
+    });
+
+    describe('getDisruptions', function () {
+        it('should call the model service', function (done) {
+            DisruptionService.getDisruptions().then(function(data){
+                expect(data).to.not.be.null;
+                expect(DisruptionModelServiceStub.getDisruptions.calledOnce).to.be.true;
+                expect(data.length).to.be.equal(1);
+                done();
+            })
+        });
+    });
+
+    describe('addDisruption', function () {
+        it('should call the model service', function (done) {
+            DisruptionService.addDisruption(1).then(function(data){
+                expect(data).to.not.be.null;
+                expect(DisruptionModelServiceStub.addDisruption.calledOnce).to.be.true;
+                expect(DisruptionModelServiceStub.addDisruption.calledWith(1)).to.be.true;
+                done();
+            })
+        });
+    });
+
+    describe('removeDisruption', function () {
+        it('should call the model service', function (done) {
+            DisruptionService.removeDisruption(1).then(function(data){
+                expect(data).to.not.be.null;
+                expect(DisruptionModelServiceStub.removeDisruption.calledOnce).to.be.true;
+                expect(DisruptionModelServiceStub.removeDisruption.calledWith(1)).to.be.true;
                 done();
             })
         });
